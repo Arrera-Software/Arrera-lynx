f80504c13dfb3f766342efbfba4670584598fb4f
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-01-13 17:05:47 +0100

    Creation de la fenetre

--- lynx_gui/arrera_lynx.py
+++ lynx_gui/arrera_lynx.py
@@ -1,3 +1,6 @@
+from lib.arrera_tk import *
+from librairy.travailJSON import *
+from librairy.dectectionOS import OS
 """
 Todo :
 - Page Acceuil (Icon de l'assistant + petit texte qui presente l'assistant+ bouton configurer)
@@ -9,6 +12,18 @@ Todo :
 - IA (Activer le mode IA, choisir et download model)
 """
 
-class arrera_lynx:
-    def __init__(self):
-        pass
+class arrera_lynx(aTk):
+    def __init__(self,conf_file:str,theme_file:str):
+        self.__json_file = jsonWork(conf_file)
+        os = OS()
+        if os.osLinux() or os.osMac():
+            icon = self.__json_file.getContentJsonFlag("icon_unix")
+        else :
+            icon = self.__json_file.getContentJsonFlag("icon_win")
+
+        super().__init__(title=f"{self.__json_file.getContentJsonFlag("name_assistant")} : Configuration",
+                         width=800,height=500,resizable=False,
+                         theme_file=theme_file,
+                         icon=icon)
+
+        self.mainloop()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from lynx_gui.arrera_lynx import*
 
 def main():
-   pass
+   arrera_lynx(conf_file="json_file/configLynx.json",
+               theme_file="asset/theme/theme_bleu_blanc.json")
 
 if __name__ == '__main__':
     main()

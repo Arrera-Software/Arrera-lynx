de6fb8c75de8627d8fd47e27a87a17506a1375db
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-01-13 16:50:54 +0100

    Creation todo

--- lynx_gui/arrera_lynx.py
+++ lynx_gui/arrera_lynx.py
@@ -1,4 +1,13 @@
-
+"""
+Todo :
+- Page Acceuil (Icon de l'assistant + petit texte qui presente l'assistant+ bouton configurer)
+- Page USER (Un zone pour nom, une zone pour prenom et Menu pour choisir le genre)
+- Partie mobiliter (Domicile Meteo / Travail meteo, adresse travaille , adresse domicile et un bouton pour ajouter un ville supplementaire pour la meteo)
+- Environement numerique (Ajout de logiciel, ajout de racourcie web et config moteur de recherche)
+- Systeme et Micro (Activer l'historique, Gerer trigerword et ajouter sons micro(activer ou non par le fichier de conf))
+- Travail (Dossier travail Arrera Work, dossier download et token github(activer ou non par le fichier de conf))
+- IA (Activer le mode IA, choisir et download model)
+"""
 
 class arrera_lynx:
     def __init__(self):

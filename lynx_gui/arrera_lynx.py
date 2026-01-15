4351127454541d8db94a2aaddaa84f80a7e85cd0
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-01-15 12:12:36 +0100

    Correction des faute d'orthographe

--- lynx_gui/arrera_lynx.py
+++ lynx_gui/arrera_lynx.py
@@ -31,7 +31,6 @@ class arrera_lynx(aTk):
         self.__ia = self.__ia_frame()
         self.__end = self.__end_frame()
 
-
         self.__welcome.placeCenter()
 
         self.mainloop()
@@ -61,7 +60,7 @@ class arrera_lynx(aTk):
         info = aFrame(m,width=350,height=200)
         lTitle = aLabel(m,text="Information de l'utilisateur",police_size=25)
         lDesc = aLabel(m,police_size=20,
-                       text="Pour apprendre a vous connectre l'assistant a besoin de savoir votre nom, prenom et le pronom que vous voulez qui vous appelle",
+                       text="Pour apprendre à vous connaître, l'assistant a besoin de savoir votre nom, prénom et le pronom que vous voulez qu'il utilise",
                        wraplength=250,justify="left")
 
         self.__eFirstName = aEntryLengend(info,text="Prénom",police_size=15)
@@ -83,11 +82,11 @@ class arrera_lynx(aTk):
     def __mobility_frame(self):
         m = aFrame(self,width=775,height=475)
 
-        lTitle = aLabel(m,text="Parametre de mobiliter",police_size=25)
+        lTitle = aLabel(m,text="Paramètres de mobilité",police_size=25)
         btn = aButton(m,text="Continuer",size=20)
 
         fMeteo = aFrame(m,width=365 ,height=300)
-        ltMeteo = aLabel(fMeteo,text="Meteo",police_size=20)
+        ltMeteo = aLabel(fMeteo,text="Météo",police_size=20)
         self.__eMDomicile = aEntryLengend(fMeteo,text="Ville de domicile",
                                           police_size=15)
         self.__eMWork = aEntryLengend(fMeteo, text="Ville de travail",
@@ -96,7 +95,7 @@ class arrera_lynx(aTk):
 
         fGPS = aFrame(m,width=365,height=300)
         ltGPS = aLabel(fGPS, text="GPS", police_size=20)
-        self.__eGDomicile = aEntryLengend(fGPS, text="Adressse de domicile",
+        self.__eGDomicile = aEntryLengend(fGPS, text="Adresse de domicile",
                                           police_size=15,width=175)
         self.__eGWork = aEntryLengend(fGPS, text="Adresse de travail",
                                       police_size=15)
@@ -121,10 +120,10 @@ class arrera_lynx(aTk):
     def __environment_frame(self):
         m = aFrame(self,width=775,height=475)
 
-        lTitle = aLabel(m,text="Parametre d'environnement numerique",police_size=25)
+        lTitle = aLabel(m,text="Paramètres d'environnement numérique",police_size=25)
 
         fWeb = aFrame(m,width=350,height=200)
-        lTWeb = aLabel(fWeb,police_size=20,text="Racourcie internet")
+        lTWeb = aLabel(fWeb,police_size=20,text="Raccourci internet")
         self.__eWebName = aEntryLengend(fWeb,text="Nom",police_size=15)
         self.__eWebLink = aEntryLengend(fWeb,text="Lien",police_size=15)
 
@@ -154,14 +153,14 @@ class arrera_lynx(aTk):
     def __search_frame(self):
         m = aFrame(self,width=775,height=475)
 
-        lTitle = aLabel(m,text="Parametre de recherche",police_size=25)
+        lTitle = aLabel(m,text="Paramètres de recherche",police_size=25)
 
-        lDesc = aLabel(m,text=f"L'assistant {self.__assistant_name} vous permet de faire des recherches sur internet. Choississez le moteur de recherche par default que vous voulez utiliser"
+        lDesc = aLabel(m,text=f"L'assistant {self.__assistant_name} vous permet de faire des recherches sur internet. Choisissez le moteur de recherche par défaut que vous voulez utiliser"
                        ,wraplength=300,justify="left",police_size=20)
 
         listEngine = self.__json_file.getFlagListJson("list_engine_search")
         fSearch = aFrame(m,width=258,height=200)
-        lTSearch = aLabel(fSearch,police_size=20,text="Moteur de recherche\npar default")
+        lTSearch = aLabel(fSearch,police_size=20,text="Moteur de recherche\npar défaut")
         self.__mSearchEngine = aOptionMenu(fSearch,value=listEngine)
 
         lTitle.placeTopCenter()
@@ -178,17 +177,17 @@ class arrera_lynx(aTk):
     def __system_frame(self):
         m = aFrame(self,width=775,height=475)
 
-        lTitle = aLabel(m,text="Parametre general de l'assistant",police_size=25)
+        lTitle = aLabel(m,text="Paramètres généraux de l'assistant",police_size=25)
 
         fSystem = aFrame(m,width=350,height=200)
-        lTSysteme = aLabel(fSystem,police_size=20,text="Parametrage de l'assistant")
+        lTSysteme = aLabel(fSystem,police_size=20,text="Paramétrage de l'assistant")
         self.__enableHist = aSwicht(fSystem,text="Activer l'historique",default_value=True)
 
         if self.__json_file.getContentJsonFlag("micro_use") == "1":
             fMicro = aFrame(m,width=350,height=200)
             lTMicro = aLabel(fMicro,police_size=20,text="Microphone")
-            self.__enableSound = aSwicht(fMicro,text="Sons au demarage de l'ecoute",default_value=True)
-            btnTriger = aButton(fMicro,text="Ajouter un mots declencheur",size=20)
+            self.__enableSound = aSwicht(fMicro,text="Sons au démarrage de l'écoute",default_value=True)
+            btnTriger = aButton(fMicro,text="Ajouter un mot déclencheur",size=20)
 
             fMicro.placeLeftCenter()
             fSystem.placeRightCenter()
@@ -211,15 +210,15 @@ class arrera_lynx(aTk):
 
     def __work_frame(self):
         m = aFrame(self,width=775,height=475)
-        lTitle = aLabel(m,text="Parametrage des dossiers",police_size=25)
+        lTitle = aLabel(m,text="Paramétrage des dossiers",police_size=25)
 
         fWork = aFrame(m,width=350,height=200)
         lTWork = aLabel(fWork,police_size=20,text="Dossier de travail")
-        bFWork = aButton(fWork,text="Enregister les dossiers\nde travail",size=20)
+        bFWork = aButton(fWork,text="Enregistrer les dossiers\nde travail",size=20)
 
         fDownload = aFrame(m,width=350,height=200)
-        lTDownload = aLabel(fDownload,police_size=20,text="Dossier de telechargement")
-        bFDownload = aButton(fDownload,text="Enregister le dossier\nde telechargement",size=20)
+        lTDownload = aLabel(fDownload,police_size=20,text="Dossier de téléchargement")
+        bFDownload = aButton(fDownload,text="Enregistrer le dossier\nde téléchargement",size=20)
 
         btn = aButton(m,text="Continuer",size=20)
 
@@ -243,13 +242,13 @@ class arrera_lynx(aTk):
 
         lTitle = aLabel(m,police_size=25,text="Token github")
 
-        lDesc = aLabel(m,text="Pour utiliser tout les fonctionnalites de codehelp vous devais enregistrer votre token github.(Stoker entiremenr de locale)",
+        lDesc = aLabel(m,text="Pour utiliser toutes les fonctionnalités de codehelp vous devez enregistrer votre token github. (Stocké entièrement en local)",
                        wraplength=300,justify="left",police_size=20)
 
         fToken = aFrame(m,width=300,height=300)
         lToken = aLabel(fToken,police_size=20,text="Enregistrement du token")
         self.__eToken = aEntryLengend(fToken,text="Token",police_size=15)
-        btnGenerateToken = aButton(fToken,text="Generer un token",size=20)
+        btnGenerateToken = aButton(fToken,text="Générer un token",size=20)
 
         btn = aButton(m,text="Continuer",size=20)
 
@@ -269,19 +268,19 @@ class arrera_lynx(aTk):
 
     def __ia_frame(self):
         m = aFrame(self,width=775,height=475)
-        lTitle = aLabel(m,police_size=25,text="Parametrage du mode IA")
+        lTitle = aLabel(m,police_size=25,text="Paramétrage du mode IA")
 
         fChooseModel = aFrame(m,width=350,height=237)
-        lTModel = aLabel(fChooseModel,police_size=20,text="Choix du modele d'IA")
-        bDModel = aButton(fChooseModel,text="Choisir un modele\na telecharger",size=20)
+        lTModel = aLabel(fChooseModel,police_size=20,text="Choix du modèle d'IA")
+        bDModel = aButton(fChooseModel,text="Choisir un modèle\nà télécharger",size=20)
 
         fDesc = aFrame(m,width=350,height=200,fg_color=m.cget("fg_color"))
-        lDesc = aLabel(fDesc,text=f"L'assistant {self.__assistant_name} integre des modele d'IA pour améliorer c'est reponse. Vous pouvais choisir le modele que vous voulez utiliser et adpater a votre ordinateur",
+        lDesc = aLabel(fDesc,text=f"L'assistant {self.__assistant_name} intègre des modèles d'IA pour améliorer ses réponses. Vous pouvez choisir le modèle que vous voulez utiliser et adapté à votre ordinateur",
                        wraplength=325,justify="left",police_size=20)
         self.__bEnableIA = aSwicht(fDesc,text="Activer le mode IA",default_value=True)
 
         if len(self.__gestUser.get_model_downloaded()) != 0:
-            bDownloadedModel = aButton(fChooseModel,text="Modele telecharger",size=20)
+            bDownloadedModel = aButton(fChooseModel,text="Modèle téléchargé",size=20)
 
             bDModel.placeCenterOnWidth(y=75)
             bDownloadedModel.placeCenterOnWidth(y=155)

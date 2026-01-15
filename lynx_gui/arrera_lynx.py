f51968dee8203fee6d73b77e44661dc302290c74
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-01-15 11:09:05 +0100

    Suppr du todo

--- lynx_gui/arrera_lynx.py
+++ lynx_gui/arrera_lynx.py
@@ -1,16 +1,6 @@
 from lib.arrera_tk import *
 from librairy.travailJSON import *
 from gestionnaire.gestion import gestionnaire
-"""
-Todo :
-- Page Acceuil (Icon de l'assistant + petit texte qui presente l'assistant+ bouton configurer)
-- Page USER (Un zone pour nom, une zone pour prenom et Menu pour choisir le genre)
-- Partie mobiliter (Domicile Meteo / Travail meteo, adresse travaille , adresse domicile et un bouton pour ajouter un ville supplementaire pour la meteo)
-- Environement numerique (Ajout de logiciel, ajout de racourcie web et config moteur de recherche)
-- Systeme et Micro (Activer l'historique, Gerer trigerword et ajouter sons micro(activer ou non par le fichier de conf))
-- Travail (Dossier travail Arrera Work, dossier download et token github(activer ou non par le fichier de conf))
-- IA (Activer le mode IA, choisir et download model)
-"""
 
 class arrera_lynx(aTk):
     def __init__(self,gest:gestionnaire,conf_file:str,theme_file:str):
